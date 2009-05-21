f0974fae9cd80f90b9d9374976e7815af4a2c2cf
Luís Guilherme Fernandes Pereira <pereira@budweiser.lsc.ic.unicamp.br>
2009-05-21 13:00:05 -0300

    migration to create paroquias

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090511194934) do
+ActiveRecord::Schema.define(:version => 20090521153750) do
 
   create_table "estados", :force => true do |t|
     t.string   "nome"

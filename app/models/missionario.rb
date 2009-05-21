38dab0b6e744e0cc7444c998869828a035ec5679
Luís Guilherme Fernandes Pereira <lpereira@gmail.com>
2009-05-21 00:07:29 -0300

    Some changes in Missionário model and view

--- app/models/missionario.rb
+++ app/models/missionario.rb
@@ -1,5 +1,6 @@
 class Missionario < ActiveRecord::Base
   belongs_to :estado
+  belongs_to :paroquia
   has_and_belongs_to_many :sacramentos
   validates_presence_of :nome, :endereco, :bairro, :cidade, :estado, :cep, :email
   validates_uniqueness_of :email

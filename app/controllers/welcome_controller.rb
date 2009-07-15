d2d2fd65fa89d54a70504e9e5045ce0ae47057dd
Luís Guilherme Fernandes Pereira <lpereira@gmail.com>
2009-07-15 17:52:28 -0300

    Mudei o texto inicial e coloquei pra aparecer o Coordenador geral na tela

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @num_missionarios = Missionario.count
-    threshold = 1
-    @coordenacao = Coordenacao.find(:all, :conditions => { :nivel => 0..threshold }, :order => "nivel ASC" )
-    @coordenadores = []
-    @coordenacao.each do |line| 
-      @coordenadores << line.missionario
-    end
+    @coordenador = Coordenacao.find(:first, :conditions => { :nivel => 0 })
   end
   def authorize
   end

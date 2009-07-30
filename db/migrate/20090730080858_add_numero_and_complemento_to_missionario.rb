0f84ab17f7658dfd73032ea240ddc41b761af72c
Luís Guilherme Fernandes Pereira <pereira@budweiser.lsc.ic.unicamp.br>
2009-07-30 05:21:06 -0300

    Migration pra separar número e complemento do endereço

---
+++ db/migrate/20090730080858_add_numero_and_complemento_to_missionario.rb
@@ -0,0 +1,36 @@
+class AddNumeroAndComplementoToMissionario < ActiveRecord::Migration
+  def self.up
+    add_column :missionarios, :numero, :string
+    add_column :missionarios, :complemento, :string
+
+    Missionario.find(:all).each do |m|
+      sepv = m.endereco.split(',')
+      l = sepv[0]
+      nc = sepv[1]
+      nc.strip! if nc != nil
+      n = nc
+      if !nc.blank?
+        nsep = nc.split(' ')
+        n = nsep[0]
+        c = nsep[1]
+      end
+      m.endereco = l
+      m.numero = n || ""
+      m.complemento = c || ""
+      m.save
+    end
+  end
+
+  def self.down
+    Missionario.find(:all).each do |m|
+      novo_endereco = m.endereco
+      novo_endereco += ", " + m.numero unless m.numero.blank?
+      novo_endereco += " " + m.complemento unless m.complemento.blank?
+      m.endereco = novo_endereco
+      m.save
+    end
+    
+    remove_column :missionarios, :complemento
+    remove_column :missionarios, :numero
+  end
+end

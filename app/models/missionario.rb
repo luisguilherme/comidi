00d2fa75135461749ddbb4ed2eb070aeb2662b33
Luís Guilherme Fernandes Pereira <pereira@budweiser.lsc.ic.unicamp.br>
2009-07-30 05:33:11 -0300

    Mudanças no missionário pra se adequar ao novo modelo do banco de dados

--- app/models/missionario.rb
+++ app/models/missionario.rb
@@ -3,7 +3,7 @@ class Missionario < ActiveRecord::Base
   belongs_to :paroquia
   belongs_to :cargo
   has_and_belongs_to_many :sacramentos
-  validates_presence_of :nome, :nome_cracha, :endereco, :bairro, :cidade, :estado, :cep
+  validates_presence_of :nome, :nome_cracha, :endereco, :bairro, :cidade, :estado, :cep, :numero
   validates_uniqueness_of :email, :allow_blank => true
   validates_numericality_of :cep
   has_one :usuario
